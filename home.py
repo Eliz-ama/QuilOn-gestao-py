097cce350a8ebc5a96d624f51c1c4be003059bf6
Eliz-ama <isefshondo@gmail.com>
2023-05-15 21:02:43 -0300

    v3

--- home.py
+++ home.py
@@ -1,15 +1,15 @@
 from tkinter import*
 from PIL import Image, ImageTk
-Login=Tk()
+home=Tk()
 
 taskBarHeight = 40
 
 #Configurações da tela
-Login.title("Acesso ao QuilOn")
-Login.resizable(False, False)
+home.title("Acesso ao QuilOn")
+home.resizable(False, False)
 
-width_screen = Login.winfo_screenwidth()
-height_screen = Login.winfo_screenheight() - taskBarHeight
+width_screen = home.winfo_screenwidth()
+height_screen = home.winfo_screenheight() - taskBarHeight
 
 width =990
 height = 600
@@ -17,85 +17,89 @@ height = 600
 posx = (width_screen / 2) - (width / 2)
 posy = (height_screen / 2) - (height / 2)
 
-Login.maxsize(width, height)
-Login.minsize(width, height)
+home.maxsize(width, height)
+home.minsize(width, height)
 
-Login.geometry("%dx%d+%d+%d" % (width, height, posx, posy))
-Login.configure(bg='#fff')
+home.geometry("%dx%d+%d+%d" % (width, height, posx, posy))
+home.configure(bg='#fff')
 
-#titulo bem-vindo
-txt_titulo = Label(Login,text="Bem-vindo" ,bg = "#FFF", font=("Helvetica 20"), foreground="#F6AA1C")
-txt_titulo.place(relx = .119, rely = .30, anchor = "n")
-
-#Estilização do Login
+#Estilização do logo
 logo_quilon_origin = Image.open("img/QuilOn.png")
 logo_resize = logo_quilon_origin.resize((210, 50), Image.ANTIALIAS)
 logoquilon = ImageTk.PhotoImage(logo_resize)
-logo_qui = Label(Login, image = logoquilon , bg="#fff")
-logo_qui.place(relx = .140, rely = .40, anchor = "n")
+logo_qui = Label(home, image = logoquilon , bg="#fff")
+logo_qui.place(relx = .505, rely = .03, anchor = "n")
 
-#botao acesse sua conta
-acesso = Button(Login, text = "Acesso a sua conta", bd = 0, bg = "#FFF", fg = "#777777", font = "Helvetica 10 underline", activebackground = "#FFF", activeforeground = "#777")
-acesso.place(relx = .120, rely = .53, anchor = "n")
+#Estilização do usuario
+usuario_origin = Image.open("img/usuario.png")
+logo_resize = usuario_origin.resize((100, 100), Image.ANTIALIAS)
+usu = ImageTk.PhotoImage(logo_resize)
+usuario = Label(home, image = usu , bg="#fff")
+usuario.place(relx = .150, rely = .20, anchor = "n")
 
-#titulo entrar
-txt_titulo = Label(Login,text="Entrar" ,bg = "#FFF", font=("Helvetica 20"), foreground="#F6AA1C")
-txt_titulo.place(relx = .460, rely = .20, anchor = "n")
-#faça login
-txt_titulo = Label(Login,text="Faça o login na sua conta" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
-txt_titulo.place(relx = .460, rely = .27, anchor = "n")
+#adm
+txt_nome = Label(home,text="Administrador(a) Elisângela da silva" ,bg = "#FFF", font=("Helvetica 10"))
+txt_nome.place(relx = .150, rely = .40, anchor = "n")
 
-#campo nome
-txt_nome = Label(Login,text="Nome" ,bg = "#FFF", font=("Helvetica 10"))
-txt_nome.place(relx = .390, rely = .35, anchor = "n")
-lbl_nome = Entry(Login)
-lbl_nome.place(relx = .465, rely = .39, anchor = "n" ,  width="190" , height="20")
+#faça cadastro
+cadastro_origin = Image.open("img/1.png")
+cadastro_resize = cadastro_origin.resize((15, 15), Image.ANTIALIAS)
+cad = ImageTk.PhotoImage(cadastro_resize)
+cadastro = Label(home, image = cad , bg="#fff")
+cadastro.place(relx = .05, rely = .50, anchor = "n")
+txt_titulo = Label(home,text="Cadastro" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
+txt_titulo.place(relx = .09, rely = .50, anchor = "n")
 
-#campo senha
-txt_valor = Label(Login,text="Senha" ,bg = "#FFF", font=("Helvetica 10"))
-txt_valor.place(relx = .390, rely = .45, anchor = "n")
-lbl_valor = Entry(Login)
-lbl_valor.place(relx = .465, rely = .49, anchor = "n",  width="190" , height="20")
+#faça quilombos
+quilombos_origin = Image.open("img/2.png")
+quilombos_resize = quilombos_origin.resize((15, 15), Image.ANTIALIAS)
+qui = ImageTk.PhotoImage(quilombos_resize)
+quilombos = Label(home, image = qui , bg="#fff")
+quilombos.place(relx = .05, rely = .55, anchor = "n")
+txt_titulo = Label(home,text="Quilombos" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
+txt_titulo.place(relx = .10, rely = .55, anchor = "n")
 
-#campo tipo de acesso
-txt_duracao = Label(Login,text="Tipo de acesso" ,bg = "#FFF", font=("Helvetica 10"))
-txt_duracao.place(relx = .413, rely = .55, anchor = "n")
-lbl_duracao = Entry(Login)
-lbl_duracao.place(relx = .465, rely = .59, anchor = "n",  width="190" , height="20")
+#faça painel de vendas
+painel_origin = Image.open("img/3.png")
+painel_resize = painel_origin.resize((20, 20), Image.ANTIALIAS)
+pai = ImageTk.PhotoImage(painel_resize)
+painel = Label(home, image = pai , bg="#fff")
+painel.place(relx = .05, rely = .60, anchor = "n")
+txt_titulo = Label(home,text="Painel de vendas" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
+txt_titulo.place(relx = .12, rely = .60, anchor = "n")
 
-#botões de Entrar
-btn_salvar=Button(Login,text="Entar", bg="#F6AA1C")
-btn_salvar.place(relx = .4650, rely = .70, anchor = "n",  width="80" , height="25")
+#faça eventos
+eventos_origin = Image.open("img/4.png")
+eventos_resize = eventos_origin.resize((15, 15), Image.ANTIALIAS)
+eve = ImageTk.PhotoImage(eventos_resize)
+eventos = Label(home, image = eve , bg="#fff")
+eventos.place(relx = .05, rely = .65, anchor = "n")
+txt_titulo = Label(home,text="Eventos" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
+txt_titulo.place(relx = .09, rely = .65, anchor = "n")
 
-# segunda parte criar contar
+#faça chat
+chat_origin = Image.open("img/5.png")
+chat_resize = chat_origin.resize((15, 15), Image.ANTIALIAS)
+cha = ImageTk.PhotoImage(chat_resize)
+chat = Label(home, image = cha , bg="#fff")
+chat.place(relx = .05, rely = .70, anchor = "n")
+txt_titulo = Label(home,text="Chat" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
+txt_titulo.place(relx = .08, rely = .70, anchor = "n")
 
-#titulo entrar
-txt_titulo = Label(Login,text="Crie sua conta" ,bg = "#FFF", font=("Helvetica 20"), foreground="#F6AA1C")
-txt_titulo.place(relx = .755, rely = .20, anchor = "n")
-#faça login
-txt_titulo = Label(Login,text="Faça o login na sua conta" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
-txt_titulo.place(relx = .755, rely = .27, anchor = "n")
+#faça configuração
+configuracoes_origin = Image.open("img/6.png")
+configuracoes_resize = configuracoes_origin.resize((15, 15), Image.ANTIALIAS)
+conf = ImageTk.PhotoImage(configuracoes_resize)
+configuracoes = Label(home, image = conf , bg="#fff")
+configuracoes.place(relx = .05, rely = .75, anchor = "n")
+txt_titulo = Label(home,text="Configuração" ,bg = "#FFF", font=("Helvetica 10 underline"), foreground="#777777")
+txt_titulo.place(relx = .10, rely = .75, anchor = "n")
 
-#campo nome
-txt_nome = Label(Login,text="Nome" ,bg = "#FFF", font=("Helvetica 10"))
-txt_nome.place(relx = .690, rely = .35, anchor = "n")
-lbl_nome = Entry(Login)
-lbl_nome.place(relx = .765, rely = .39, anchor = "n" ,  width="190" , height="20")
+#imagem
+imagem_origin = Image.open("img/7.png")
+imagem_resize = imagem_origin.resize((500, 500), Image.ANTIALIAS)
+img = ImageTk.PhotoImage(imagem_resize)
+imagem = Label(home, image = img , bg="#fff")
+imagem.place(relx = .700, rely = .15, anchor = "n")
 
-#campo email
-txt_valor = Label(Login,text="E-mail" ,bg = "#FFF", font=("Helvetica 10"))
-txt_valor.place(relx = .690, rely = .45, anchor = "n")
-lbl_valor = Entry(Login)
-lbl_valor.place(relx = .765, rely = .49, anchor = "n",  width="190" , height="20")
-
-#campo senha
-txt_duracao = Label(Login,text="Senha" ,bg = "#FFF", font=("Helvetica 10"))
-txt_duracao.place(relx = .690, rely = .55, anchor = "n")
-lbl_duracao = Entry(Login)
-lbl_duracao.place(relx = .765, rely = .59, anchor = "n",  width="190" , height="20")
-
-#botao cadastre-se
-btn_salvar=Button(Login,text="Cadastre-se", bg="#F6AA1C")
-btn_salvar.place(relx = .765, rely = .70, anchor = "n",  width="80" , height="25")
-
-Login.mainloop()
+home.mainloop()
